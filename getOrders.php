cd0f77e9fabb644766586a652f904166e3fefc25
Omar Kobrosli <okobrosli2@gmail.com>
2025-08-18 10:55:52 +0300

    Added an API to show orders.

---
+++ getOrders.php
@@ -0,0 +1,27 @@
+<?php
+
+include("./connection.php");
+
+$email = $_POST["email"];
+
+$stmt = $pdo->prepare("SELECT id FROM users WHERE email = ?");
+$stmt->execute([$email]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user) {
+    $id = $user['id'];
+}
+
+$stmt = $pdo->prepare("SELECT name,price,description FROM products inner join order_items on order_items.prod_id = products.prod_id
+inner join orders on order_items.order_id = orders.order_id  WHERE orders.user_id = ?");
+$stmt->execute([$id]);
+
+$order = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (!$order) {
+  echo json_encode([
+    "message" => "no orders found"
+  ]);
+} else {
+  echo json_encode($order);
+}
