a29d56a0750b92803428fa0a7fa5abe9df93dde7
Omar Kobrosli <okobrosli2@gmail.com>
2025-08-18 10:37:03 +0300

    Added an API to create an order.

---
+++ createOrder.php
@@ -0,0 +1,45 @@
+<?php
+
+include("./connection.php");
+
+$email = $_POST["email"];
+
+$stmt = $pdo->prepare("SELECT id FROM users WHERE email = ?");
+$stmt->execute([$email]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user) {
+    $id = $user['id'];
+}
+
+$stmt = $pdo->prepare("SELECT cart_id FROM carts WHERE user_id = ?");
+$stmt->execute([$id]);
+$cart_id = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($cart_id) {
+    $cartId = $cart_id['cart_id'];
+}
+
+$stmt = $pdo->prepare("SELECT prod_id, quantity FROM cart_items WHERE cart_id = ?");
+$stmt->execute([$cartId]);
+$cartItems = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $pdo->prepare("INSERT INTO orders (user_id) VALUES (?)");
+$stmt->execute([$id]);
+$orderId = $pdo->lastInsertId();
+
+$stmt = $pdo->prepare("INSERT INTO order_items (order_id, prod_id, quantity) VALUES (?, ?, ?)");
+foreach ($cartItems as $item) {
+    $stmt->execute([$orderId, $item['prod_id'], $item['quantity']]);
+}
+
+$stmt = $pdo->prepare("DELETE FROM cart_items WHERE cart_id = ?");
+    $stmt->execute([$cartId]);
+
+if($stmt)
+{
+    echo "Order created.";
+}
+else{
+    echo "failed to create order.";
+}
