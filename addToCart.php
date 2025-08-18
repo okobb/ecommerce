548680f98c0cec8fd2919c850f4f819443787190
Omar Kobrosli <okobrosli2@gmail.com>
2025-08-18 10:01:30 +0300

    Fixed add to cart API to allow adding items to separate carts according to user.

--- addToCart.php
+++ addToCart.php
@@ -2,11 +2,25 @@
 
 include("./connection.php");
 
-$cartId = $_POST["cartid"];
+$email = $_POST["email"];
 $prodId = $_POST["prodid"];
 $quantity = $_POST["quantity"];
 
-$stmt = $pdo->prepare("INSERT INTO cart_items (cart_id, prod_id, quantity) VALUES (?, ?, ?)");
-$stmt->execute([$cartId, $prodId, $quantity]);
+$stmt = $pdo->prepare("SELECT id FROM users WHERE email = ?");
+$stmt->execute([$email]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-echo "Item added to cart. ";
+if ($user) {
+    $id = $user['id'];
+}
+
+$stmt = $pdo->prepare("SELECT cart_id FROM carts WHERE user_id = ?");
+$stmt->execute([$id]);
+$cart_id = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($cart_id) {
+    $cartId = $cart_id['cart_id'];
+}
+$stmt = $pdo->prepare("Insert INTO cart_items(cart_id,prod_id,quantity) values(?,?,?)");
+$stmt->execute([$cartId,$prodId,$quantity]);
+
